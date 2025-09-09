0aacc36cd6679e17d51f9f2d788896c2ea34015a
megamanpro <megamanpro@gmail.com>
2025-09-08 23:38:45 -0400

    Update assignment1_part2.py

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,6 +1,16 @@
-{
- "cells": [],
- "metadata": {},
- "nbformat": 4,
- "nbformat_minor": 5
-}
+class Book:
+    author = ""
+    title = ""
+
+    def __init__(self, author, title):
+        self.author = author
+        self.title = title
+
+    def display(self):
+        print(f"{self.title}, written by {self.author}")
+
+book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+book2 = Book("Walter Scott", "Ivanhoe: A Romance")
+
+book1.display()
+book2.display()
